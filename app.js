d3728c3d3a49566bf5be18e214b68daa8f0cfa5c
Mahad366 <mahad.younis@gigalabs.co>
2023-01-17 15:26:16 +0500

    PORT instead of port

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ mongoose
     useUnifiedTopology: true,
     useCreateIndex: true,
   })
-  .then(() => app.listen(process.env.port || 3000))
+  .then(() => app.listen(process.env.PORT || 8000))
   .catch((err) => console.log(err));
 
 // routes
